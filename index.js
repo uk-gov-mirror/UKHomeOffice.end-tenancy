7a2c186faa29e4a699b1aeff52e16821a3310d46
Sarah-Jane Luff <sarah-jane.luff@digital.homeoffice.gov.uk>
2021-04-01 11:53:07 +0100

    Remove Generic Locals from index.js file

--- index.js
+++ index.js
@@ -13,32 +13,9 @@ const options = {
   redis: config.redis
 };
 
-const addGenericLocals = (req, res, next) => {
-  res.locals.htmlLang = 'en';
-  res.locals.feedbackUrl = '/feedback';
-  res.locals.footerSupportLinks = [
-    { path: '/cookies', property: 'base.cookies' },
-    { path: '/terms-and-conditions', property: 'base.terms' },
-    { path: '/accessibility', property: 'base.accessibility' },
-  ];
-  return next();
-};
-
 if (process.env.NODE_ENV !== 'production') {
   options.middleware = [require('./mocks')];
 }
 
-const app = bootstrap(options);
-
-app.use((req, res, next) => addGenericLocals(req, res, next));
-app.use('/terms-and-conditions', (req, res, next) => {
-  res.locals = Object.assign({}, res.locals, req.translate('terms'));
-  next();
-});
-
-app.use('/cookies', (req, res, next) => {
-  res.locals = Object.assign({}, res.locals, req.translate('cookies'));
-  next();
-});
-
+bootstrap(options);
 
