0a8e6ddc1adfe9a9a896eb06ef6ea0cb4f050c6f
Sarah-Jane Luff <sarah-jane.luff@digital.homeoffice.gov.uk>
2021-04-01 09:07:13 +0100

    removed session and added app.use

--- index.js
+++ index.js
@@ -10,14 +10,32 @@ const options = {
   ],
   getCookies: false,
   getTerms: false,
-  redis: config.redis,
-  session: {
-    'name': 'hof.sid'
-  },
+  redis: config.redis
+};
+
+const addGenericLocals = (req, res, next) => {
+  res.locals.htmlLang = 'en';
+  res.locals.feedbackUrl = '/feedback';
+  res.locals.footerSupportLinks = [
+    { path: '/cookies', property: 'base.cookies' },
+    { path: '/terms-and-conditions', property: 'base.terms' },
+    { path: '/accessibility', property: 'base.accessibility' },
+  ];
+  return next();
 };
 
 if (process.env.NODE_ENV !== 'production') {
   options.middleware = [require('./mocks')];
 }
 
-bootstrap(options);
+const app = bootstrap(options);
+
+app.use((req, res, next) => addGenericLocals(req, res, next));
+app.use('/terms-and-conditions', (req, res, next) => {
+  res.locals = Object.assign({}, res.locals, req.translate('terms'));
+  next();
+});
+app.use('/cookies', (req, res, next) => {
+  res.locals = Object.assign({}, res.locals, req.translate('cookies'));
+  next();
+});
