14a3c04d8d04b0cbf840865098d89a46534fc7bc
Rhodine Orleans-Lindsay <43888758+Rhodine-orleans-lindsay@users.noreply.github.com>
2024-03-08 20:38:09 +0000

    HOFF-689: Update UKVI ending tenancy to version of hof using axios
    - update hof version in package.json
    - update notifications-node-client to fix vulnerabilities
    - update playwright to fix failing acceptance test
    - update yarn.lock
    - refactor upload model to use axios
    - update snyk ignore list
    - remove anchore steps in drone as it is to be replaced by trivy
    - add hof logger
    - update node version to version 20
    - remove 'UAT' from deploy_to_branch message

--- apps/end-tenancy/models/upload.js
+++ apps/end-tenancy/models/upload.js
@@ -2,69 +2,69 @@
 'use strict';
 
 const url = require('url');
-const Model = require('hof').model;
+const FormData = require('form-data');
+const { model: Model } = require('hof');
 const config = require('../../../config');
-const debug = require('debug')('upload-model');
+const logger = require('hof/lib/logger')({ env: config.env });
 
 module.exports = class UploadModel extends Model {
-  save() {
-    return new Promise((resolve, reject) => {
+  async save() {
+    try {
       const attributes = {
         url: config.upload.hostname
       };
       const reqConf = url.parse(this.url(attributes));
-      reqConf.formData = {
-        document: {
-          value: this.get('data'),
-          options: {
-            filename: this.get('name'),
-            contentType: this.get('mimetype')
-          }
-        }
-      };
-      reqConf.method = 'POST';
-      debug('SAVE PDF DATA', reqConf);
-      this.request(reqConf, (err, data) => {
-        if (err) {
-          return reject(err);
-        }
-        debug('RESPONSE FROM FILE VAULT SAVE', err, data);
-        return resolve(data);
+      const formData = new FormData();
+      formData.append('document', this.get('data'), {
+        filename: this.get('name'),
+        contentType: this.get('mimetype')
       });
-    });
+      reqConf.data = formData;
+      reqConf.method = 'POST';
+      reqConf.headers = {
+        ...formData.getHeaders()
+      };
+      logger.log('info', 'SAVE PDF DATA', reqConf);
+      const response = await this.request(reqConf);
+      logger.log('info', 'RESPONSE FROM FILE VAULT SAVE', response);
+
+      await this.set({ url: response.url });
+      await this.unset('data');
+
+      return response;
+    } catch (err) {
+      logger.error('Error in save method ', err);
+      throw err;
+    }
   }
 
-  auth() {
-    if (!config.keycloak.tokenUrl) {
-      // eslint-disable-next-line no-console
-      console.error('keycloak token url is not defined');
-      return Promise.resolve({
-        bearer: 'abc123'
-      });
+  async auth() {
+    try {
+      if (!config.keycloak.tokenUrl) {
+        logger.error('keycloak token url is not defined');
+        return {
+          bearer: 'abc123'
+        };
+      }
+      const tokenReq = {
+        url: config.keycloak.tokenUrl,
+        headers: { 'content-type': 'application/x-www-form-urlencoded' },
+        data: {
+          username: config.keycloak.username,
+          password: config.keycloak.password,
+          grant_type: 'password',
+          client_id: config.keycloak.clientId,
+          client_secret: config.keycloak.clientSecret
+        },
+        method: 'POST'
+      };
+      logger.log('info', 'REQUEST DATA', tokenReq);
+      const response = await this._request(tokenReq);
+      logger.log('info', 'RESPONSE FROM FILE VAULT', response);
+      return { bearer: response.data.access_token };
+    } catch (err) {
+      logger.error(`Error in auth method: ${err.response?.data?.error} - ${err.response?.data?.error_description}`);
+      throw err;
     }
-    const tokenReq = {
-      url: config.keycloak.tokenUrl,
-      form: {
-        username: config.keycloak.username,
-        password: config.keycloak.password,
-        grant_type: 'password',
-        client_id: config.keycloak.clientId,
-        client_secret: config.keycloak.clientSecret
-      },
-      method: 'POST'
-    };
-
-    return new Promise((resolve, reject) => {
-      debug('REQUEST DATA', tokenReq);
-      this._request(tokenReq, (err, res) => {
-        debug('RESPONSE FROM FILE VAULT', err, res);
-        if (err) {
-          return reject(err);
-        }
-        return resolve({
-          bearer: JSON.parse(res.body).access_token
-        });
-      });
-    });
   }
 };
